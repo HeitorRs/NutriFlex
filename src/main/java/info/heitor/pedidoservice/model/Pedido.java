59f5e3c69fb6a23852773e79019535235ef1e15c
HeitorRs <jabuticabadefer@gmail.com>
2024-09-04 21:32:28 -0300

    Adicionando Mensageria

--- src/main/java/info/heitor/pedidoservice/model/Pedido.java
+++ src/main/java/info/heitor/pedidoservice/model/Pedido.java
@@ -9,12 +9,21 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Random;
 
 @Document(collection = "pedidos")
 @Data@AllArgsConstructor@NoArgsConstructor@Builder
 public class Pedido {
     @Id
     private String id;
-    private List<ItemPedido> items;
+    private List<ItemPedido> itens;
     private BigDecimal valorTotal;
+    private String cupom;
+    private BigDecimal valorTotalComDesconto;
+    private PedidoStatus status;
+    private Long serial;
+
+    public void criarSerial(){
+        this.serial = new Random().nextLong(3000);
+    }
 }
--- src/main/java/info/heitor/pedidoservice/service/ProdutoService.java
+++ src/main/java/info/heitor/pedidoservice/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package info.heitor.pedidoservice.service;
 
 import info.heitor.pedidoservice.model.Produto;
+import info.heitor.pedidoservice.service.feign.ProdutoClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -8,9 +9,10 @@ import org.springframework.web.client.RestClient;
 @Service
 @RequiredArgsConstructor
 public class ProdutoService {
+
+    private final ProdutoClient produtoClient;
+
     public Produto findById(Long id) {
-        RestClient restClient = RestClient.create();
-        var serverUrl = String.format("http://localhost:8080/produtos/%d", id);
-        return restClient.get().uri(serverUrl).retrieve().toEntity(Produto.class).getBody();
+        return produtoClient.findById(id);
     }
 }
